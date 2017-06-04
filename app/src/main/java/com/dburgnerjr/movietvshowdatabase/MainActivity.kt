118b161db92352dd005e2ce1625afdd8bf08f198
Daniel Burgner <dburgnerjr@yahoo.com>
2017-06-03 21:11:55 -0400

    Completed Part 2 of Kotlin tutorial.
    Completed Part 2 of Kotlin tutorial.
    https://medium.com/@juanchosaravia/learn-kotlin-while-developing-an-andr
    oid-app-part-2-e53317ffcbe9

--- app/src/main/java/com/dburgnerjr/movietvshowdatabase/MainActivity.kt
+++ app/src/main/java/com/dburgnerjr/movietvshowdatabase/MainActivity.kt
@@ -1,12 +1,38 @@
 package com.dburgnerjr.movietvshowdatabase
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v4.app.FragmentManager
+import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.Toolbar
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val toolbar = findViewById(R.id.toolbar) as Toolbar
+        setSupportActionBar(toolbar)
+    }
+
+    fun changeFragment(f: Fragment, bCleanStack: Boolean = false) {
+        val ft = supportFragmentManager.beginTransaction();
+        if (bCleanStack) {
+            clearBackStack();
+        }
+        ft.setCustomAnimations(
+                R.anim.abc_fade_in, R.anim.abc_fade_out, R.anim.abc_popup_enter, R.anim.abc_popup_exit);
+        ft.replace(R.id.activity_base_content, f);
+        ft.addToBackStack(null);
+        ft.commit();
+    }
+
+    fun clearBackStack() {
+        val mgr = supportFragmentManager;
+        if (mgr.backStackEntryCount > 1) {
+            mgr.popBackStack();
+        } else {
+            finish();
+        }
     }
 }
